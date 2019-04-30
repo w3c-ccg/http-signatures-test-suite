71e037c3ee4e804d2f56fd2871ce1167067458fd
andrewjones <ajones@digitalbazaar.com>
2019-04-30 13:04:51 -0500

    Add eslint to the project.

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  env: {
+    node: true,
+    mocha: true
+  },
+  extends: [
+    'eslint-config-digitalbazaar',
+    'eslint-config-digitalbazaar/jsdoc'
+  ]
+};
---
+++ test/http-signatures-v11/10-basic.js
@@ -0,0 +1,5 @@
+const config = require('../../config.json');
+const chai = require('chai');
+const {expect} = chai;
+const util = require('./util');
+const { hasType } = util;
