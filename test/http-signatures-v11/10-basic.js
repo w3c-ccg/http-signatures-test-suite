9320fca7e6dceaae2a99f882cffd02be113dbf82
andrewjones <ajones@digitalbazaar.com>
2019-04-30 13:13:46 -0500

    Add basic test.

--- test/http-signatures-v11/10-basic.js
+++ test/http-signatures-v11/10-basic.js
@@ -2,4 +2,21 @@ const config = require('../../config.json');
 const chai = require('chai');
 const {expect} = chai;
 const util = require('./util');
-const { hasType } = util;
+
+// configure chai
+const should = chai.should();
+chai.use(require('chai-as-promised'));
+
+describe('Basic Requests', function() {
+  let generatorOptions = null;
+  before(function() {
+    generatorOptions = {
+      generator: config.generator,
+      args: ''
+    };
+  });
+  it('should return a valid signature', async function() {
+    const result = await util.generate('basic-request.json', generatorOptions);
+    console.log('result', result);
+  });
+});
