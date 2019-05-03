d51c2a01fa81d056de19c202e4e57045ced375be
andrewjones <ajones@digitalbazaar.com>
2019-05-03 12:44:08 -0500

    Use cat filePath - to append latest date header.

--- test/v11/util.js
+++ test/v11/util.js
@@ -9,13 +9,13 @@ const readFile = util.promisify(fs.readFile);
 async function generate(file, options) {
   options = options || {};
   const filePath = path.join(__dirname, 'input', file);
-  let httpMessage = await readFile(filePath, 'utf8');
   const date = options.date || new Date().toGMTString();
   const latestDate = `date: ${date}`;
-  httpMessage += latestDate;
   const headers = `--headers ${options.headers.join(',') || ''} `;
-  const {stdout, stderr} = await exec(
-    options.generator + ' ' + options.args + headers + '"' + httpMessage + '"');
+  // this cat filePath - the dash is the last pipe op
+  const httpMessage = `echo ${latestDate} | cat ${filePath} -`;
+  const {stdout, stderr} = await exec(httpMessage + ' | ' +
+    options.generator + ' ' + options.args + headers);
   console.log(stdout, stderr);
   if(stderr) {
     throw new Error(stderr);
