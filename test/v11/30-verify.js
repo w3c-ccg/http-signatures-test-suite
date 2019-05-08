a1098dd3a31c408c3399034d9d38afb0b9e435f7
andrewjones <ajones@digitalbazaar.com>
2019-05-08 17:55:38 -0500

    Add more signature tests.

--- test/v11/30-verify.js
+++ test/v11/30-verify.js
@@ -1,8 +1,34 @@
+const config = require('../../config.json');
+const util = require('./util');
+
 describe('should verify http message', function() {
+  let generatorOptions = null;
+  before(function() {
+    generatorOptions = {
+      generator: config.generator,
+      command: 'verify',
+      args: {},
+      date: new Date().toGMTString(),
+    };
+  });
+
   it('should conform to 2.1.1 - fail if no keyId paramater', function() {
 
   });
   it('should conform to 2.1.2 - fail if no signature paramater', function() {
 
+  });
+
+  it.skip('conform to 2.2 - Ambiguous Parameters', async function() {
+    /**
+      * If any of the parameters listed above are erroneously duplicated in
+      * the associated header field, then the last parameter defined MUST be
+      * used.
+    */
+    const result = await util.generate(
+      'duplicate-headers-request.txt', generatorOptions);
+    result.should.not.be.null;
+    result.should.be.a('string');
+    result.should.equal('duplicate: last\n');
   });
 });
