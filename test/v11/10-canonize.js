c74f4071a5109e9c4cb5219991718965d3c23013
andrewjones <ajones@digitalbazaar.com>
2019-05-13 16:59:23 -0400

    Remove need to specify file type httpMessage from generate.

--- test/v11/10-canonize.js
+++ test/v11/10-canonize.js
@@ -13,10 +13,10 @@ describe('Canonize', function() {
       date: new Date().toGMTString(),
     };
   });
-  it('return a valid signature string', async function() {
+  it('return a valid signature string.', async function() {
     generatorOptions.args.headers = ['date'];
     const result = await util.generate(
-      'basic-request.httpMessage', generatorOptions);
+      'basic-request', generatorOptions);
     result.should.not.be.null;
     result.should.be.a('string');
     result.should.equal(
@@ -29,7 +29,7 @@ describe('Canonize', function() {
     */
       generatorOptions.args.headers = ['Digest', 'Host'];
       const result = await util.generate(
-        'default-test.httpMessage', generatorOptions);
+        'default-test', generatorOptions);
       expect(result, 'Expected a result').to.exist;
       let expected =
         'digest: SHA-256=X48E9qOokqqrvdts8nOJRJN3OWDUoyWxBf7kbu9DBPE=\n';
@@ -52,7 +52,7 @@ describe('Canonize', function() {
 
   it(`If the header field name is (request-target) then generate
       the header field value by concatenating the lowercased :method,
-      an ASCII space, and the :path pseudo-headers`, async function() {
+      an ASCII space, and the :path pseudo-headers.`, async function() {
     /**
      * If the header field name is `(request-target)` then generate
      * the header field value by concatenating the lowercased :method,
@@ -63,14 +63,14 @@ describe('Canonize', function() {
     */
     generatorOptions.args.headers = ['(request-target)'];
     const result = await util.generate(
-      'basic-request.httpMessage', generatorOptions);
+      'basic-request', generatorOptions);
     expect(result, 'Expected a result').to.exist;
     result.should.be.a('string');
     const expected = '(request-target): get /basic/request\n';
     result.should.equal(expected, 'expected signature string to match');
   });
 
-  it('- 4. should change capitalized Headers to lowercase', async function() {
+  it('SHOULD change capitalized Headers to lowercase.', async function() {
     /**
       * Create the header field string by concatenating the lowercased
       * header field name followed with an ASCII colon `:`,
@@ -81,12 +81,13 @@ describe('Canonize', function() {
      */
   });
 
-  it.skip('should return an empty String if headers is empty', async function() {
-    const result = await util.generate(
-      'invalidheaders-request.httpMessage', generatorOptions);
-    expect(result, 'Expected a result').to.exist;
-    result.should.be.an('object');
-  });
+  it.skip(
+    'SHOULD return an empty String if headers is empty.', async function() {
+      const result = await util.generate(
+        'invalidheaders-request', generatorOptions);
+      expect(result, 'Expected a result').to.exist;
+      result.should.be.an('object');
+    });
 
   it.skip(`If the header parameter is not specified,
       implementations MUST operate as if the field were specified with a
@@ -98,7 +99,7 @@ describe('Canonize', function() {
         * single value, `(created)`, in the list of HTTP headers.
       */
     const result = await util.generate(
-      'basic-request.httpMessage', generatorOptions);
+      'basic-request', generatorOptions);
     expect(result, 'Expected a result').to.exist;
     result.should.be.a('string');
     const expected = `created: ${generatorOptions.date}\n`;
@@ -127,7 +128,7 @@ describe('Canonize', function() {
           let error = null;
           try {
             await util.generate(
-              `created-${algorithm}.httpMessage`, generatorOptions);
+              `created-${algorithm}`, generatorOptions);
           } catch(e) {
             error = e;
           }
@@ -150,7 +151,7 @@ describe('Canonize', function() {
         generatorOptions.args.headers = [`(${param})`];
         let error = null;
         try {
-          await util.generate(`not-${param}.httpMessage`, generatorOptions);
+          await util.generate(`not-${param}`, generatorOptions);
         } catch(e) {
           error = e;
         }
@@ -174,27 +175,26 @@ describe('Canonize', function() {
         let error = null;
         try {
           await util.generate(
-            `${param}-not-int.httpMessage`, generatorOptions);
+            `${param}-not-int`, generatorOptions);
         } catch(e) {
           error = e;
         }
         expect(error, 'Expected an error to be thrown').to.exist;
       });
-      it.skip(`SHOULD return (${param})`,
+      it.skip(`SHOULD return (${param}).`,
         async function() {
           generatorOptions.args.headers = [`(${param})`];
           const result = await util.generate(
-            `${param}.httpMessage`, generatorOptions);
+            `${param}`, generatorOptions);
           const expected = `(${param}): 1\n`;
           expect(result, 'Expected a result').to.exist;
           result.should.be.a('string');
           result.should.equal(expected, 'expected signature string to match');
         });
 
-    it(`The client MUST use the values of each HTTP header field in the headers
-        Signature Parameter, in the order they appear in the headers
-        Signature Parameter.`,
-    async function() {
+      it(`The client MUST use the values of each HTTP header field
+        in the headers Signature Parameter, in the order they appear
+        in the headers Signature Parameter.`, async function() {
       /**
         * In order to generate the string that is signed with a key, the client
         * MUST use the values of each HTTP header field in the `headers`
@@ -203,7 +203,7 @@ describe('Canonize', function() {
        */
         generatorOptions.args.headers = ['Content-Length', 'Host'];
         const result = await util.generate(
-          'default-test.httpMessage', generatorOptions);
+          'default-test', generatorOptions);
         expect(result, 'Expected a result').to.exist;
         result.should.be.a('string');
         let expected = 'content-length: 18\n';
@@ -211,11 +211,10 @@ describe('Canonize', function() {
         result.should.equal(expected, 'expected signature string to match');
       });
 
-    it(`All header field values associated with the
-        header field MUST be concatenated, separated by an
-        ASCII comma and an ASCII space , and used in the
-        order in which they will appear in the transmitted HTTP message.`,
-    async function() {
+      it(`All header field values associated with the header field
+        MUST be concatenated, separated by an ASCII comma and an ASCII space,
+        and used in the order in which they will appear in the
+        transmitted HTTP message.`, async function() {
       /**
         * If there are multiple instances of the same header
         * field, all header field values associated with the
@@ -225,9 +224,8 @@ describe('Canonize', function() {
       */
       });
 
-      it(`If a header specified in the headers parameter
-        cannot be matched with a provided
-        header in the message, the implementation
+      it(`If a header specified in the headers parameter cannot be
+        matched with a provided header in the message, the implementation
         MUST produce an error.`, async function() {
       /**
         * If a header specified in the headers parameter is
@@ -238,7 +236,7 @@ describe('Canonize', function() {
         let error = null;
         try {
           generatorOptions.args.headers = ['not-in-request'];
-          await util.generate('basic-request.httpMessage', generatorOptions);
+          await util.generate('basic-request', generatorOptions);
         } catch(e) {
           error = e;
         }
@@ -256,7 +254,7 @@ describe('Canonize', function() {
         generatorOptions.args.headers = ['Server'];
         try {
           await util.generate(
-            'malformed-request.httpMessage', generatorOptions);
+            'malformed-request', generatorOptions);
         } catch(e) {
           error = e;
         }
