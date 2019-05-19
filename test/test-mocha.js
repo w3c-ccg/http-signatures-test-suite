15940aaab63950b6900163520ba17d714eb1fab3
andrewjones <ajones@digitalbazaar.com>
2019-05-19 14:32:33 -0400

    Update reports to use handlebars and a custom mocha reporter.
    Clean up and remove node.
    
    Remove need for destructuring result of makeMaster.
    
    Use custom mocha reporter to create results.
    
    Organize tests by subtests.
    
    Add sections for each test.
    
    Grey out optional.
    
    Set Editor's Draft in reSpec.

--- test/test-mocha.js
+++ test/test-mocha.js
@@ -1,5 +1,7 @@
 const chai = require('chai');
-
+const mocha = require('mocha');
+const ImplementationReporter = require('./ImplementationReporter');
 // configure chai
 chai.should();
 
+mocha.reporters.ImplementationReporter = ImplementationReporter;
